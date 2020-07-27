a4d743e3c9a6240129766b96486160363a4cd4cb
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-26 20:20:50 -0400

    Create celebratecredentials.py

---
+++ celebratecredentials.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# This file works in conjunction with a discord bot by storing the clientID, client secret, and token for the bot registered with Discord.
+
+ClientID = ""
+ClientSecret = ""
+Token = ""
