6ecb0da7ab5718542d54fba94c4eecf111cf301e
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-27 12:15:50 -0400

    Update celebrate.py

--- celebrate.py
+++ celebrate.py
@@ -14,7 +14,7 @@ from manuallog import *
 
 # Clients are the connection to Discord websocket and API.
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.Client.
-class bot(discord.client):
+class bot(discord.Client):
 
     bolNotCelebrated = True
 
@@ -32,19 +32,19 @@ class bot(discord.client):
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.on_message
     async def on_message(self, message):
 
-        # If the celebrate messages have not been sent, call the celebrate function.  If they have, setup for the next month.
-        if (time.strftime('%d') == 1):
-            if bolNotCelebrated:
-            celebrate()
-        else:
-            bolNotCelebrated == true
-            celebrate()  #debugging call
-
         # Ignores messages sent by the bot.
         if message.author.id == self.user.id:
             return
 
-        # Send PM to Phoenix to register a new celebration log.
+        # If the celebrate messages have not been sent, call the celebrate function.  If they have, setup for the next month.
+        if (time.strftime('%d') == 1):
+            if bolNotCelebrated:
+                celebrate()
+        else:
+            bolNotCelebrated = True
+            celebrate()  #debugging call
+
+        # Respnd to celebrate messages and send PM to BOT manager to register a new celebration log.
         if message.content.startswith('Maia, celebrate'):
             userAuthor = message.author.mention
             await message.channel.send('Will do, {}'.format(userAuthor))
@@ -53,7 +53,6 @@ class bot(discord.client):
 
 # Instantiates classes
 client = bot()
- 
 
 # Starts bot with token from credentials file.  This must be the last command in the file.
 client.run(Token)
