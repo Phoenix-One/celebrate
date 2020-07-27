dc76d5afe852712c6eddc1f800cf6ecc71b20fc7
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-27 11:41:48 -0400

    Update manuallog.py

--- manuallog.py
+++ manuallog.py
@@ -1,3 +1,67 @@
 #!/usr/bin/python3
 
-# This file works in conjunction with a discord bot by storing data that cannot be kept in a public venue.
+def celebrate():
+
+    month = time.strftime('%B')
+    print(month)
+
+    if month == "January":
+        January()
+    elif month == "February":
+        February()
+    elif month == "March":
+        March()
+    elif month == "April":
+        April()
+    elif month == "May":
+        May()
+    elif month == "June":
+        June()
+    elif month == "July":
+        July()
+    elif month == "August":
+        August()
+    elif month == "September":
+        September()
+    elif month == "October":
+        October()
+    elif month == "November":
+        November()
+    elif month == "December":
+        December()
+    
+def January():
+     print('January')
+
+def February():
+    print('February')
+
+def March():
+    print('March')
+
+def April():
+    print('April')
+
+def May():
+    print('May')
+
+def June():
+    print('June')
+
+def July(discord.client):
+    print('July')
+
+def August():
+    print('August')
+
+def September():
+    print('September')
+
+def October():
+    print('October')
+
+def November():
+    print('November')
+
+def December():
+    print('December')
