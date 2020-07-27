bee65208c7ddc929d7b59638b97a878b16a067dc
Phoenix <38627831+Phoenix-One@users.noreply.github.com>
2020-07-27 11:41:12 -0400

    Update celebrate.py

--- celebrate.py
+++ celebrate.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+# Imports basic python function libraries
+import time
+
 # Imports discord functions.  See https://pypi.org/project/discord.py/
 import discord
 
 # Imports file (celebratecredentials.py) that stores bot credentials (clientID, client secret, and token)
 from celebratecredentials import *
 
+# Imports manuallog.py, where the speciic events are stored.
+from manuallog import *
+
 # Clients are the connection to Discord websocket and API.
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.Client.
-class bot(discord.Client):
+class bot(discord.client):
+
+    bolNotCelebrated = True
 
 # Provides bot name and ID upon successful log-in.    
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.on_ready
@@ -24,17 +32,28 @@ class bot(discord.Client):
 # https://discordpy.readthedocs.io/en/latest/api.html#discord.on_message
     async def on_message(self, message):
 
+        # If the celebrate messages have not been sent, call the celebrate function.  If they have, setup for the next month.
+        if (time.strftime('%d') == 1):
+            if bolNotCelebrated:
+            celebrate()
+        else:
+            bolNotCelebrated == true
+            celebrate()  #debugging call
+
         # Ignores messages sent by the bot.
-        
         if message.author.id == self.user.id:
             return
 
-        # Returns "World" when receiving "Hello" message
-        if message.content.startswith('Hello'):
-            await message.channel.send('World')
+        # Send PM to Phoenix to register a new celebration log.
+        if message.content.startswith('Maia, celebrate'):
+            userAuthor = message.author.mention
+            await message.channel.send('Will do, {}'.format(userAuthor))
+            userPhoenix = client.get_user(203445784899878912)
+            await userPhoenix.send('Phoenix, {} said: " {}" on serverID {} - channelID {}.  Their userID is: {}'.format(message.author, message.content, message.guild.id, message.channel.id, message.author.id))
 
-# Instantiates client
+# Instantiates classes
 client = bot()
+ 
 
 # Starts bot with token from credentials file.  This must be the last command in the file.
 client.run(Token)
